1db2b9e9804953ade28133e82a4fb3013eb81dbf
Yongsoon Ko <goys5228@gmail.com>
2020-01-02 15:37:12 +0900

    여기까지 순탄하게 진행함

---
+++ 012-10951.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution() {
+  int ans = 0;
+  return ans;
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+  // for(;;) {
+  //   int a, b;
+  //   cin >> a >> b;
+  //   cout << a + b << '\n';
+  // }
+}
